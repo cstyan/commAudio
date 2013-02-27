c9f128c0c47ac0fd5d85c19b4e63cdf14ea035f2
Steve Lo <kashun_lo@hotmail.com>
2013-02-27 11:26:09 -0800

    the client

---
+++ Client/Client/client.h
@@ -0,0 +1,13 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#define IDM_TEST        1       /* menu items ID's */
+#define IDM_QUIT        10
+
+#define PLAY_BUTTON     1000
+#define PAUSE_BUTTON	1001
+#define SONG_DISPLAY	1002
+
+LRESULT CALLBACK WndProc (HWND, UINT, WPARAM, LPARAM) ;
+
+#endif
